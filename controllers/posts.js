f74cd8d71207ba2d0977f36bbb965073b598a166
Alanna <alanna.noguchi@students.makeschool.com>
2020-06-24 20:27:11 -0700

    users must be logged in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,25 +3,30 @@ const Post = require('../models/post');
 module.exports = (app) => {
 
     // INDEX
-    app.get('/', (req, res) => {
-        Post.find({}).lean().then(posts => {
-            res.render("posts-index", { posts });
-        }).catch(err => {
+    app.get("/", (req, res) => {
+        const currentUser = req.user;
+      
+        Post.find({})
+          .then(posts => {
+            res.render("posts-index", { posts, currentUser });
+          })
+          .catch(err => {
             console.log(err.message);
-        });
-    });
+          });
+      });
 
     
     // CREATE
-    app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
-
-        // SAVE INSTANCE OF POST MODEL TO DB
-        post.save((err, post) => {
-        // REDIRECT TO THE ROOT
-            return res.redirect(`/`);
-        })
+    app.post("/posts/new", (req, res) => {
+        if (req.user) {
+            const post = new Post(req.body);
+        
+            post.save(function(err, post) {
+                return res.redirect(`/`);
+            });
+        } else {
+            return res.status(401); // UNAUTHORIZED
+        }
     });
 
     // New Post Route with async/await
