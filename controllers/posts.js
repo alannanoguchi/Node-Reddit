caf26dc4cf79b5324d354eb416ab5c4d273ca073
Alanna <alanna.noguchi@students.makeschool.com>
2020-06-24 23:19:24 -0700

    Authors for comments are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -57,8 +57,8 @@ module.exports = (app) => {
     app.get("/posts/:id", function (req, res) {
         var currentUser = req.user;
         // LOOK UP THE POST
-
-        Post.findById(req.params.id).populate('comments').populate('author')
+    
+        Post.findById(req.params.id).populate({path:'comments', populate: {path: 'author'}}).populate('author')
             .then(post => {
                 res.render("posts-show", { post, currentUser });  
             })
